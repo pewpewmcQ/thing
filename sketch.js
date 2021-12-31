d85448705f7a357cdd5d8a37dce3e57bce553009
pewpewmcQ <93830526+pewpewmcQ@users.noreply.github.com>
2021-12-31 05:20:58 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var student={
+name:"BOB",
+class:6,
+rollnum:7,
+fav:"math",
+marks:[10,50,38,69,42]
+};
+
+
+function setup() {
+  createCanvas(400, 400);
+  console.log(student.marks);
+}
+
+function draw() {
+  background(220);
+}
